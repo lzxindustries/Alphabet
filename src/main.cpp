e9f2a8688d67f84ca8f5ba09677ebab148474c57
Lars Larsen <lars@lzxindustries.net>
2020-11-02 19:56:30 -0800

    Edit main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include <QApplication>
 #include <QPushButton>
 #include <version.h>
+#include <string>
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
 
     //QPushButton button("Version: " + gGIT_VERSION);
-    QPushButton button("Version: ");
+    std::string versionString = "Version: ";
+    versionStirng.append(gGIT_VERSION);
+    QPushButton button(versionString);
     button.show();
 
     return app.exec();
