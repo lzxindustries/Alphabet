f45595049c31ee06dbb8a36dfd62ee3aa1a1a1ac
Lars Larsen <lars@lzxindustries.net>
2020-11-02 20:15:23 -0800

    Edit main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
     QApplication app(argc, argv);
 
     //QPushButton button("Version: " + gGIT_VERSION);
-    QPushButton button(gGIT_VERSION.c_str());
+    QPushButton button("Test Button");
     button.show();
 
     return app.exec();
